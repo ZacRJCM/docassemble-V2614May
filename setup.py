49415550607efbd403fd0342dff9fdf9566d532a
Zachary Caldicott-Millard <100551712+ZacRJCM@users.noreply.github.com>
2022-05-14 02:00:31 +0930

    K2

---
+++ setup.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from setuptools import setup, find_packages
+from fnmatch import fnmatchcase
+from distutils.util import convert_path
+
+standard_exclude = ('*.pyc', '*~', '.*', '*.bak', '*.swp*')
+standard_exclude_directories = ('.*', 'CVS', '_darcs', './build', './dist', 'EGG-INFO', '*.egg-info')
+def find_package_data(where='.', package='', exclude=standard_exclude, exclude_directories=standard_exclude_directories):
+    out = {}
+    stack = [(convert_path(where), '', package)]
+    while stack:
+        where, prefix, package = stack.pop(0)
+        for name in os.listdir(where):
+            fn = os.path.join(where, name)
+            if os.path.isdir(fn):
+                bad_name = False
+                for pattern in exclude_directories:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                if os.path.isfile(os.path.join(fn, '__init__.py')):
+                    if not package:
+                        new_package = name
+                    else:
+                        new_package = package + '.' + name
+                        stack.append((fn, '', new_package))
+                else:
+                    stack.append((fn, prefix + name + '/', package))
+            else:
+                bad_name = False
+                for pattern in exclude:
+                    if (fnmatchcase(name, pattern)
+                        or fn.lower() == pattern.lower()):
+                        bad_name = True
+                        break
+                if bad_name:
+                    continue
+                out.setdefault(package, []).append(prefix+name)
+    return out
+
+setup(name='docassemble.V2614May',
+      version='0.0.1',
+      description=('Environmental Defenders Office Application'),
+      long_description="# docassemble.EDO1\r\nEnvironmental Defenders Office application titled 'Tree Ark'\r\n## Authors\r\nJordan Buttery, butt0158@flinders.edu.au\r\nLachlan Cromb, crom0038@flinders.edu.au\r\nAdarsh Jacob, jaco0194@flinders.edu.au\r\nRobert Cousins, cous0060@flinders.edu.au\r\nZachary Caldicott-Millard, cald0093@flinders.edu.au",
+      long_description_content_type='text/markdown',
+      author='Law in a Digital Age EDO1',
+      author_email='butt0158@flinders.edu.au',
+      license='The MIT License (MIT)',
+      url='https://github.com/Butt0158/docassemble-EDO1',
+      packages=find_packages(),
+      namespace_packages=['docassemble'],
+      install_requires=[],
+      zip_safe=False,
+      package_data=find_package_data(where='docassemble/V2614May/', package='docassemble.V2614May'),
+     )
+
